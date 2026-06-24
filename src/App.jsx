96b7f76c1ee3a5a8c7b973ccbe6a374e63965f80
unknown <sandeep@blitzy.com>
2026-06-24 17:54:49 +0530

    initial react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <h2>About Me</h2>
+      <p>I am learning React.</p>
+    </div>
+  );
+}
+
+export default App;
